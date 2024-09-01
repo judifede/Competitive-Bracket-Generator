b03687c44a9e4193ffe2958aaafabe12c399ad31
Diego Fernández <judifede217@gmail.com>
2024-09-01 11:47:15 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Competitive Bracket Generator</h1>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
